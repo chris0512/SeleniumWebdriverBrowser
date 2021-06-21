9ca27b74fcd9ffe3f79a4ebbab686b8129cb6a2f
Chris0512 <kyang3200@gmail.com>
2021-06-21 16:52:09 +0800

    auto fill the search bar

--- interaction.py
+++ interaction.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "C:\\Development\\chromedriver_win32\\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
@@ -6,6 +7,14 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 article_count = driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]')
-print(article_count.text)
+# article_count.click()
 
-driver.quit()
+
+all_portals = driver.find_element_by_link_text("All portals")
+# all_portals.click()
+
+search = driver.find_element_by_name("search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
+
+# driver.quit()
